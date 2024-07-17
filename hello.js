5f3ebae8eb58726f66bfebbc7d27c10702056985
yeahwouh <michaelauer1010@gmail.com>
2024-07-17 12:18:36 +0200

    the grid is done

--- hello.js
+++ hello.js
@@ -1,1 +1,23 @@
-console.log("hello");
+const cont = document.querySelector("div")
+
+
+for(let i=0; i<16; i++) {
+    let rowDiv = document.createElement("div")
+    // rowDiv.style.width = 100px;
+    // rowDiv.style.height = 100px;
+    for(let j=0; j<16; j++) {
+        let columnDiv = document.createElement("div")
+        columnDiv.style.width = "50px";
+        columnDiv.style.height = "50px";
+        columnDiv.style.backgroundColor = "darkcyan"
+        columnDiv.style.borderStyle = "solid"
+        columnDiv.style.borderColor = "black"
+        columnDiv.setAttribute("id", `${j}:${i}`)
+        rowDiv.appendChild(columnDiv)
+    }
+    cont.appendChild(rowDiv)
+}
+
+cont.style.display = "flex"
+cont.style.flexDirection = "row"
+
