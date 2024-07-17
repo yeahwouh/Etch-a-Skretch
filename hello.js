33b0f90364aeb6a2d774efc55ce507aba702541c
yeahwouh <michaelauer1010@gmail.com>
2024-07-17 13:04:46 +0200

    added the button at the top and centered it

--- hello.js
+++ hello.js
@@ -1,23 +1,40 @@
 const cont = document.querySelector("div")
 
+const btn = document.createElement("button")
+btn.textContent = "REFRESH"
+btn.style.display = "block"
+
+cont.appendChild(btn)
+
+btn.style.margin = '0 auto 20px'
+
+const div = document.createElement("div")
+
+cont.appendChild(div)
 
 for(let i=0; i<16; i++) {
-    let rowDiv = document.createElement("div")
-    // rowDiv.style.width = 100px;
-    // rowDiv.style.height = 100px;
+    let columnDiv = document.createElement("div")
     for(let j=0; j<16; j++) {
-        let columnDiv = document.createElement("div")
-        columnDiv.style.width = "50px";
-        columnDiv.style.height = "50px";
-        columnDiv.style.backgroundColor = "darkcyan"
-        columnDiv.style.borderStyle = "solid"
-        columnDiv.style.borderColor = "black"
-        columnDiv.setAttribute("id", `${j}:${i}`)
-        rowDiv.appendChild(columnDiv)
+        let rowDiv = document.createElement("div")
+        rowDiv.style.width = "50px";
+        rowDiv.style.height = "50px";
+        rowDiv.style.backgroundColor = "darkcyan"
+        rowDiv.style.borderStyle = "solid"
+        rowDiv.style.borderColor = "black"
+        rowDiv.setAttribute("id", `${j}:${i}`)
+        rowDiv.setAttribute("class", "subdiv")
+        columnDiv.appendChild(rowDiv)
     }
-    cont.appendChild(rowDiv)
-}
+    div.appendChild(columnDiv)
 
-cont.style.display = "flex"
-cont.style.flexDirection = "row"
+}
+div.style.display = "flex"
+
+div.style.flexDirection = "row"
+document.querySelectorAll('.subdiv').forEach(item => {
+    item.addEventListener('mouseover', event => {
+        item.style.backgroundColor = "#eec0c8"
+    })
+
+})
 
